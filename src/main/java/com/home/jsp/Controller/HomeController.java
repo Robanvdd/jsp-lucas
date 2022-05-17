f5096dd0555a1951c3dd5fd9cba5db7b818bf42c
Robbert-Jan van der Dennen <hidden@secret.eu>
2022-05-17 21:49:38 +0200

    initial commit

---
+++ src/main/java/com/home/jsp/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.home.jsp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping({"/", "/home"})
+    public String home() {
+        return "home";
+    }
+}
